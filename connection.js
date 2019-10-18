818bc289b90c3d3b87c3c8a369deceae082b4805
shivanichauhan18 <shivanic18@navgurukul.org>
2019-10-18 22:42:01 +0530

    creating file then made module for sales.js

---
+++ connection.js
@@ -0,0 +1,11 @@
+var knex=require("knex")({
+    client:"mysql",
+    connection:{
+        host     : '127.0.0.1',
+        user: 'root',
+        database: 'salesTax',
+        password: 'shivani123'
+    }
+})
+
+module.exports=knex
