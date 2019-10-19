48bd2caccde1573ad07772419d8f979d167fa5a1
shivanichauhan18 <shivanic18@navgurukul.org>
2019-10-19 16:33:16 +0530

    This is my all refectoring adding product list

--- salesTax.js
+++ salesTax.js
@@ -12,24 +12,20 @@ function getTax(price,tax){
     
 app.post("/add-products",function(req,res){
     categoreis=["book","food","medicine"]
-    categoreis=["book","food","medicine"]
     productDetails={"Product":req.body.Product,"imported":req.body.imported,"category":req.body.category,"Price":req.body.Price}
-
     var tax=0.0
     if(productDetails.imported){
-        tax=tax+5.0
-        if (categoreis.includes(productDetails["category"])){
-            tax=tax
-        }else{
-            tax=tax+10.0
-        }    
-    }if(!categoreis.includes(productDetails["category"])){
-        tax=tax+10.0
-    }
-    productTax=getTax(productDetails["Price"],tax)
+        tax=tax+getTax(productDetails["Price"],5)
 
-    productDetails["Tax"]=productTax
-    productDetails["TotalPrice"]=productDetails["Price"]+productTax
+    }if(!categoreis.includes(productDetails["category"])){
+        tax=tax+getTax(productDetails["Price"],10)
+    }
+
+    productDetails["Tax"]=tax
+    data=productDetails.imported
+    importing=data.toString()
+    productDetails["imported"]=importing
+    productDetails["TotalPrice"]=productDetails["Price"]+tax
     knex.select("*").from("productBill").insert(productDetails).then((result)=>{
         res.send("product Inserted")
     }).then((result)=>{
@@ -37,10 +33,8 @@ app.post("/add-products",function(req,res){
     }).catch((err)=>{
         res.send(err)
     });
-
 });
     
-
 app.listen(8000,() =>{
     console.log("listining 8000 ")
 });
