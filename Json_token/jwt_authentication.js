d180a117d94989fad76fb4692fcc62f07dd4459c
Alka05 <alka19@navgurukul.org>
2019-11-10 12:45:24 +0530

    This is my jwt authentication log-in code folder

---
+++ Json_token/jwt_authentication.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+let jwt = require('jsonwebtoken');
+var app = express();
+
+app.post('/api/posts',verifyToken,(req,res,next)=>{
+    jwt.verify((req.token),"SECRET_KEY",(err,authData)=>{
+       if(err){
+           res.sendStatus(403);
+        }else{
+        res.json({
+        message:"post created....",
+        authData
+    });
+    }
+    })
+});
+
+app.post("/login",(req,res,next)=>{
+    const user={
+        id:1,
+        name:"shivani",
+        email:"shivanic18@navgurukul.org"
+    }
+    jwt.sign(user,"SECRET_KEY",(err,token)=>{
+        res.json({
+            token:"Bearer " + token
+        })
+    })
+})
+
+
+///Token verification function.
+function verifyToken(req,res,next){
+    const bearerHeader = req.headers['authorization'];
+
+    if(typeof bearerHeader !== 'undefined'){
+        const bearerToken = bearerHeader.split(' ')[1];
+        req.token=bearerToken; 
+        next();
+
+    }else{
+       res.sendStatus(403);
+    }
+}
+
+
+
+app.listen(5000,()=>{
+    console.log("server started on port 5000")
+})
